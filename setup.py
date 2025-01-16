a757e584642ba8eed5575ebe70dad38afb2b767e
Clément ROUMILHAC <clement.roumilhac@edu.devinci.fr>
2025-01-16 16:34:44 +0100

    ajout du setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Quizz-Roumilhac-CDOF4',
+ version='1.0',
+ author='burgerclem',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
